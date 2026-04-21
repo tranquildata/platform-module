c8c8100dbd96d4228fa6e897c332c37d08cbb431
Trek Palmer <tsp@tranquildata.com>
2026-04-21 10:38:15 -0400

    File I/O multifile example

---
+++ examples/fileio/module/output.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2026, Tranquil Data, Inc. All rights reserved.
+ */
+
+static const char SAMPLE_COMMAND[] = "a,b,c,d\nsample1,sample2,sample3,sample4";
+
+static const char SAMPLE_HOUSEKEEPING[] = "e,f,g,h,i\n,1,2,3,4,5\n6,7,8,9,A";
+
+static const char SAMPLE_OUTPUT[] = "col1,col2,col3\n1,2,3\n4,5,6\n7,8,9\n,10,11,12";
